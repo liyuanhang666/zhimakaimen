29cbc8827ab3d7f5d66c6c4a429b64f18af76c47
liyuanhang666 <852633514@qq.com>
2017-12-25 20:43:21 +0800

    xingqi
    qi mo

---
+++ xing  qi/main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{  int a;
+scanf("%d",&a);
+    switch(a)
+    {
+    case 0:
+        printf("Mon\n");
+        break;
+    case 1:
+        printf("Tue\n");
+        break;
+    case 2:
+        printf("Wed\n");
+        break;
+    case 3:
+        printf("Thu\n");
+        break;
+    case 4:
+        printf("Fri\n");
+        break;
+    case 5:
+        printf("Sat\n");
+        break;
+    case 6:
+        printf("Sun\n");
+        break;
+    }
+
+
+return 0;
+}
